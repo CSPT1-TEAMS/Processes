cb3e2dd1ebad36c0b4cf82d5041ade975900e34c
unknown <alexisreyes90@gmail.com>
2018-10-03 23:15:28 -0400

    ex5 implemented

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,7 +16,39 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
-    
+    char pipeBuffer[MSGSIZE];
+    int pipePointers[2];
+
+    if(pipe(pipePointers) < 0)
+    {
+        printf("Nothing to pipe through.");
+        exit(1);
+    }
+
+    int rc = fork();
+
+    if(rc < 0){
+        printf("Fork failed");
+        exit(2);
+
+    } else if(rc == 0){
+        printf("CHILD HERE\n");
+        write(pipePointers[1], msg1, MSGSIZE);
+        write(pipePointers[1], msg2, MSGSIZE);
+        write(pipePointers[1], msg3, MSGSIZE);
+
+
+    } else {
+        wait(NULL);
+        close(pipePointers[1]);
+        printf("PARENT HERE\n");
+
+        while(read(pipePointers[0], pipeBuffer, MSGSIZE) > 0)
+        {
+            printf("Reading Pipe in parent %s\n", pipeBuffer);
+        }
+
+    }
     return 0;
+
 }
